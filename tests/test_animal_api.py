a1e6d59b7b04348ed26180710ac31d5d8c70d4fd
Ng Lai <nglaikoin@hotmail.com>
2024-04-16 21:20:08 -0300

    mudando a pag incial

--- tests/test_animal_api.py
+++ tests/test_animal_api.py
@@ -8,7 +8,7 @@ async def test_read_main():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Bem-vindo à API de Consulta sobre Animais"}
+    assert response.json() == {"message": "Bem-vindo à API de Consulta sobre Animais de um Zoo"}
 
 @pytest.mark.asyncio
 async def test_get_animals():
